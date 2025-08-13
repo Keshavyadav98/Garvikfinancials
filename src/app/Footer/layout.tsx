5d48244900db3379f8e1989b9f6d0595acce3b93
Keshavyadav98 <1998keshavyadav@gmail.com>
2025-08-13 21:47:14 +0530

    some change

--- src/app/Footer/layout.tsx
+++ src/app/Footer/layout.tsx
@@ -10,7 +10,7 @@ import { FaSquareInstagram } from "react-icons/fa6";
 
 export default function Footer() {
   return (
-    <div className='bg-black lg:mt-[33vw] dark:bg-[#061127] lg:dark:border dark:border-t-white p-0 mt-5'>
+    <div className='bg-black lg:mt-[33vw] dark:bg-[#061127] lg:dark:border dark:border-t-white p-5 mt-5'>
     <div className='flex flex-col md:flex-row lg:flex-row justify-evenly py-10'>
         <div className='w-full md:w-65 lg:w-65 h-auto p-0   flex flex-col justify-evenly'>
             {/* <Image className='mx-auto w-50' src={} alt='logo' /> */}
