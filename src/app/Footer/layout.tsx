fef82ba17f48da2f68f2e1eb64c6daa2d824adf6
Keshavyadav98 <1998keshavyadav@gmail.com>
2025-08-28 19:42:55 +0530

    font change, style

--- src/app/Footer/layout.tsx
+++ src/app/Footer/layout.tsx
@@ -11,13 +11,13 @@ import Image from 'next/image';
 
 export default function Footer() {
   return (
-    <div className='bg-black dark:bg-[#061127] lg:dark:border dark:border-t-white'>
+    <div className='bg-black dark:bg-[#061127] lg:dark:border dark:border-t-white py-2'>
     <div className='flex flex-col md:flex-row lg:flex-row'>
         <div className='w-full md:w-65 lg:w-65 h-auto p-0   flex flex-col justify-evenly'>
             <Image src={logo} alt='...' className=''  />
         </div>
         
-          <p className='font-mono text-white m-auto text-lg hover:text-[#68685f] cursor-pointer decoration-[#ba181b] text-base  font-mono text-white text-lg'>+91-8860611200</p>
+          <p className='font-mono text-white m-auto  text-lg hover:text-[#68685f] cursor-pointer decoration-[#ba181b] text-base  font-mono text-white text-lg'>+91-8860611200</p>
     </div>
     </div>
   )
